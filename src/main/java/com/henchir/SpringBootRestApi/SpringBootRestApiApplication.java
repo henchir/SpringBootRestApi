4eb98cbaf99d7ade49a9988e9345b981189d3144
Mohamed <Mohamed@192.168.1.46>
2018-04-24 12:18:00 +0200

    c

--- src/main/java/com/henchir/SpringBootRestApi/SpringBootRestApiApplication.java
+++ src/main/java/com/henchir/SpringBootRestApi/SpringBootRestApiApplication.java
@@ -5,8 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 
 @SpringBootApplication
+@EnableSwagger2 //this
 public class SpringBootRestApiApplication extends SpringBootServletInitializer {
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
